d9d09a9517101c9c6fbf0f866506fc89d39705b2
Adam Demaree <demareeadam1@gmail.com>
2017-04-06 19:54:46 -0400

    mod 3 controller

---
+++ Desktop/GitHub/RazorLessons/Razor/RazorIntro/Controllers/RazorPracticeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorIntro.Controllers
+{
+    public class RazorPracticeController : Controller
+    {
+        // GET: RazorPractice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
